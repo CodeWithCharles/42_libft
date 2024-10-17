631436345cbc89204a6925a03b96b55fdaef9550
Charles Poulain <cpoulain@c1r6p9.42lehavre.fr>
2024-10-17 10:45:15 +0200

    ~ | ft_calloc handled division by 0 and overflow

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: cpoulain <cpoulain@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 16:13:16 by cpoulain          #+#    #+#             */
-/*   Updated: 2024/10/16 17:32:38 by cpoulain         ###   ########.fr       */
+/*   Updated: 2024/10/17 10:43:52 by cpoulain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
+	if (nmemb == 0)
+		return (malloc(0));
+	if (SIZE_MAX / nmemb < size)
+		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr)
 		ft_bzero(ptr, nmemb * size);
